565a74f019c22082509be777eaf75053f7aa72e5
emerson-argueta <emerson@emersonargueta.com>
2020-11-19 02:58:19 +0000

    added test cases for lead

--- referral-tracking/lead/lead.test.ts
+++ referral-tracking/lead/lead.test.ts
@@ -1,10 +1,64 @@
+/* Test Case for leads 
+ Seven test cases so far - one for each time the Lead status is meant to change. 
+ 1. If Partner initializes Lead, then Lead is considered Open
+ 2. If Owner accepts the initialized Lead, Lead status is considered Scheduled
+ 3. If email is sent to Customer, Lead is considered Proposal Sent
+ 4. If Customer responds NO to email, then Lead is considered Lost
+ 5. If Customer responds YES to email, then Lead is considered Won
+ 6. If Partner has been paid and transaction is on-time, Lead is considered Paid , and then Closed
+ 7. If transaction is reoccuring, Lead is Closed after final payment.
+*/
+
 // describe creates a block that groups together several related tests
-describe("test", () => {
+describe("test lead status", () => {
     test("should update status to open", async () => {
-        // a lead status will become open when a referral-partner inputs it into the system
-        const lead = { "status": "open" } //createLead()
+        // 1. If Partner initializes Lead, then Lead is considered Open
+        const lead = { "status": "open" }
         const status = lead.status
 
         expect(status).toBe("open");
     });
+    test("should update status to scheduled", async () => {
+        // 2. If Owner accepts the initialized Lead, Lead status is considered Scheduled
+        const lead = { "status": "scheduled" }
+        const status = lead.status
+
+        expect(status).toBe("scheduled");
+    });
+    test("should update status to proposal-sent", async () => {
+        // 3. If email is sent to Customer, Lead is considered Proposal Sent
+        const lead = { "status": "proposal-sent" }
+        const status = lead.status
+
+        expect(status).toBe("proposal-sent");
+    });
+    test("should update status to list", async () => {
+        // 4. If Customer responds NO to email, then Lead is considered Lost
+        const lead = { "status": "lost" }
+        const status = lead.status
+
+        expect(status).toBe("lost");
+    });
+    test("should update status to won", async () => {
+        // 5. If Customer responds YES to email, then Lead is considered Won
+        const lead = { "status": "won" }
+        const status = lead.status
+
+        expect(status).toBe("won");
+    });
+    test("should update status to closed", async () => {
+        // 6. If Partner has been paid and transaction is on-time, Lead is considered Paid , and then Closed
+        const lead = { "status": "closed" }
+        const status = lead.status
+
+        expect(status).toBe("closed");
+    });
+    test("should update status to closed", async () => {
+        // 7. If transaction is reoccuring, Lead is Closed after final payment.
+        const lead = { "status": "closed" }
+        const status = lead.status
+
+        expect(status).toBe("closed");
+    });
+
 });
